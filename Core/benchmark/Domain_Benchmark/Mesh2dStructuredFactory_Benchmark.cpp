60689083e1ffaa653f9d26918b0b6da92fa2721d
PhilTaylor <Phil@DESKTOP-CIL6N53>
2025-06-29 10:25:50 +0100

    Expanded scope of Mesh2dStructuredFactory benchmark.

--- Core/benchmark/Domain_Benchmark/Mesh2dStructuredFactory_Benchmark.cpp
+++ Core/benchmark/Domain_Benchmark/Mesh2dStructuredFactory_Benchmark.cpp
@@ -11,19 +11,33 @@ using namespace fstim;
 
 static void BM_Mesh2dStructuredFactory_Domain(benchmark::State& state)
 {	
-	int cellsPerSide = state.range(0);
+	vecp::Vec2i size = vecp::Vec2i(state.range(0), state.range(1));
 	Mesh2dStructuredFactory factory = Mesh2dStructuredFactory();
 	for (auto _ : state) 
 	{
 		std::unique_ptr<Mesh2d> mesh = factory(
-			vecp::Vec2i(cellsPerSide, cellsPerSide), vecp::Vec2d(10., 10.)
+			size, vecp::Vec2d(10., 10.)
 		);
 	}
 
-	state.counters["Total Cells"] = cellsPerSide * cellsPerSide;
+	state.counters["Total Cells"] = size.x * size.y;
 }
 
 BENCHMARK(BM_Mesh2dStructuredFactory_Domain)
-	->RangeMultiplier(2)
-	->Range(8, 1024)
+	->Args({ 8,  8})
+	->Args({ 16, 16})
+	->Args({ 32, 32})
+	->Args({ 64, 64})
+	->Args({ 128, 128})
+	->Args({ 256, 256})
+	->Args({ 512, 512 })
+	->Args({ 1024, 1024 })
+	->Args({ 2,   32 })
+	->Args({ 4,   64 })
+	->Args({ 8,  128 })
+	->Args({ 16,  256 })
+	->Args({ 32,  512 })
+	->Args({ 64, 1024 })
+	->Args({ 128, 2048 })
+	->Args({ 256, 4096 })
 	->Unit(benchmark::kMillisecond);
